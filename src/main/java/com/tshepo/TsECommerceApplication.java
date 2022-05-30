fb83e2bbc830e33034058d2fe28eccc11db6c3ed
Tshepo Mokoena <tshepo.william3652@gmail.com>
2022-05-30 16:36:34 +0200

    first commit

---
+++ src/main/java/com/tshepo/TsECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.tshepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tshepo/TsECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tshepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
