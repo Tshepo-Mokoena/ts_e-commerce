56f65c1f63ad23ee60c314a4553bf7f9886c099d
Tshepo Mokoena <tshepo.william3652@gmail.com>
2022-06-09 20:13:36 +0200

    created IOrderItemService{}, OrderItemService{} classe

---
+++ src/main/java/com/tshepo/service/impl/OrderItemService.java
@@ -0,0 +1,10 @@
+package com.tshepo.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.tshepo.service.IOrderItemService;
+
+@Service
+public class OrderItemService implements IOrderItemService{
+
+}
