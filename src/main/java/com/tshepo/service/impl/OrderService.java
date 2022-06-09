8c1f80d698529168230ad126104e9a175a4282be
Tshepo Mokoena <tshepo.william3652@gmail.com>
2022-06-09 20:33:13 +0200

    dependency injection orderRepository();

--- src/main/java/com/tshepo/service/impl/OrderService.java
+++ src/main/java/com/tshepo/service/impl/OrderService.java
@@ -1,10 +1,20 @@
 package com.tshepo.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tshepo.persistence.repository.IOrderRepository;
 import com.tshepo.service.IOrderService;
 
 @Service
 public class OrderService implements IOrderService{
+	
+	private IOrderRepository orderRepository;
+	
+	@Autowired
+	private void setOrderService(IOrderRepository orderRepository) 
+	{
+		this.orderRepository = orderRepository;
+	}
 
 }
