9aa73c64a3d5ece7c23850499bf0424150268be4
Tshepo Mokoena <tshepo.william3652@gmail.com>
2022-06-09 17:23:14 +0200

    impl clearCart(){}, updateCart(){}

--- src/main/java/com/tshepo/service/impl/CartService.java
+++ src/main/java/com/tshepo/service/impl/CartService.java
@@ -1,5 +1,39 @@
 package com.tshepo.service.impl;
 
-public class CartService {
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tshepo.persistence.Cart;
+import com.tshepo.persistence.repository.ICartRepository;
+import com.tshepo.service.ICartService;
+
+@Service
+public class CartService implements ICartService{
+	
+	private ICartRepository cartRepository;
+	
+	@Autowired
+	private void setCartService(ICartRepository cartRepository) 
+	{
+		this.cartRepository = cartRepository;
+	}
+
+	@Override
+	public Cart updateCart(Cart cart) 
+	{		
+		cart.setUpdatedAt(LocalDateTime.now());
+		return cartRepository.save(cart);
+	}
+
+	@Override
+	public void clearCart(Cart cart) 
+	{
+		cart.setCartProducts(null);
+		cart.setTotal(BigDecimal.ZERO);
+		cartRepository.save(cart);
+	}
 
 }
