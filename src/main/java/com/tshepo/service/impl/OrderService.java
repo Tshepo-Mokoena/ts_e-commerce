84964161f5e08b56bacda744e434aa3fd8c2ac56
Tshepo Mokoena <tshepo.william3652@gmail.com>
2022-06-09 17:28:26 +0200

    created class OrderService{}, interface IOrderService{}

---
+++ src/main/java/com/tshepo/service/impl/OrderService.java
@@ -0,0 +1,10 @@
+package com.tshepo.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.tshepo.service.IOrderService;
+
+@Service
+public class OrderService implements IOrderService{
+
+}
