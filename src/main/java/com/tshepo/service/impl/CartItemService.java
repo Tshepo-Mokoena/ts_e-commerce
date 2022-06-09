8bdf41892699563b170ee9ceb765b33556e60fb9
Tshepo Mokoena <tshepo.william3652@gmail.com>
2022-06-09 20:25:27 +0200

    dependency injection cartItemRepository();

--- src/main/java/com/tshepo/service/impl/CartItemService.java
+++ src/main/java/com/tshepo/service/impl/CartItemService.java
@@ -1,10 +1,20 @@
 package com.tshepo.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tshepo.persistence.repository.ICartItemRepository;
 import com.tshepo.service.ICartItemService;
 
 @Service
 public class CartItemService implements ICartItemService{
+	
+	private ICartItemRepository cartItemRepository;
+	
+	@Autowired
+	private void setCartItemService(ICartItemRepository cartItemRepository) 
+	{
+		this.cartItemRepository = cartItemRepository;
+	}
 
 }
