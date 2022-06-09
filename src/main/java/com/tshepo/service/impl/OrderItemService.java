a25c853df397d31130e0f5ef975b829bb41c3b04
Tshepo Mokoena <tshepo.william3652@gmail.com>
2022-06-09 20:17:17 +0200

    injected orderItemRepository(){}

--- src/main/java/com/tshepo/service/impl/OrderItemService.java
+++ src/main/java/com/tshepo/service/impl/OrderItemService.java
@@ -1,10 +1,20 @@
 package com.tshepo.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tshepo.persistence.repository.IOrderItemRepository;
 import com.tshepo.service.IOrderItemService;
 
 @Service
 public class OrderItemService implements IOrderItemService{
+	
+	private IOrderItemRepository orderItemRepository;
+	
+	@Autowired
+	private void setOrderItemService(IOrderItemRepository orderItemRepository) 
+	{
+		this.orderItemRepository = orderItemRepository;
+	}
 
 }
